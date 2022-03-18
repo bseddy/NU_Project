9e3d2741ae969b4ddfb7f67e36662a7e4e4baf72
mmmkimchi <100895921+bseddy@users.noreply.github.com>
2022-03-17 19:41:25 -0700

    Update Project_Working.py

--- Project_Working.py
+++ Project_Working.py
@@ -9,8 +9,14 @@ print('At least 2 lowercase letters')
 print('At least 2 numbers')
 print('At least 2 special characters')
 
+# Create the variable to be used as the loop condition, when resume does not equal 'true' the loop will end
+resume = 'true'
+
+# Create the while loop (loop will start initially because resume starts as 'true')
+while resume == 'true':
+
 # Ask user to input their password and set user_password variable to their input
-user_password = input('Enter your password to verify its strength: ')
+    user_password = input('Enter a password to verify its strength: ')
 
 # if statement to verify the users input matches the criteria of a strong password and print that the password is strong, otherwise print the password is not strong
 # re.match will define a pattern of specified criteria to be matched, then we will pass the user input to match against the regex pattern
@@ -23,11 +29,20 @@ user_password = input('Enter your password to verify its strength: ')
 # The special character criteria is defined manually by typing out all of the special characters to match. ***NOTE: If a user types a special character that's not defined here it will not match and tell them their password is not strong
 # The .{8,} means match any character for a minimum of 8 characters with no maximum amount of characters (this is how we meet the criteria for 8 character minimum)
 # Each set of criteria was tested individually before being combined into one statement
-# The site https://regex101.com was a great learning tool - you can validate input to test the regex
+# The site https://regex101.com is very useful for learning regex and validating input
 
-if re.match('(?=.*[A-Z]{2})(?=.*[a-z]{2})(?=.*[0-9]{2})(?=.*[!@#$%^&*()?<>_+=]{2}).{8,}', user_password):
-    print('Password meets criteria for a STRONG password')
+    if re.match('(?=.*[A-Z]{2})(?=.*[a-z]{2})(?=.*[0-9]{2})(?=.*[!@#$%^&*()?<>_+=]{2}).{8,}', user_password):
+
+# If match is found then print message telling user password is strong
+        print('Password IS STRONG!')
+
+# Since the password is strong, the user's objective is complete, setting the resume variable to anything other than 'true' will end the loop
+        resume = 'false'
+
+# Input statement to ensure the user sees the message that their password is strong. Without this the program would close too fast before the human eye could read it.
+# Since nothing follows this line in the loop, and we changed the variable so the loop will end, the program will close after the user presses enter.
+        input('Press the [ENTER] to continue...')
 
 # else statement, ALL defined password criteria must match, otherwise tell user their password is not strong enough
-else:
-    print('Password DOES NOT meet criteria for a strong password!')
+    else:
+        print('Password is not strong enough - Try Again!')
